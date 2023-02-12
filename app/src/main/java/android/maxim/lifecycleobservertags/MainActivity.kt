512de758aac4a8a8a20a609ef65366780fa36158
MaximGuljaev <maximuspo4ta@gmail.com>
2023-02-12 17:08:17 +0500

    initial commit

---
+++ app/src/main/java/android/maxim/lifecycleobservertags/MainActivity.kt
@@ -0,0 +1,43 @@
+package android.maxim.lifecycleobservertags
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("lifecycle", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("lifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("lifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("lifecycle", "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("lifecycle", "onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("lifecycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("lifecycle", "onDestroy")
+    }
+}
